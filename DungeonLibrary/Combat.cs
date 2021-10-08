df0005a6bf035b135d5f0a8ee3e475611942da2f
Max Smith <maxsmith3169@gmail.com>
2021-10-08 15:40:25 -0500

    Combat page completed , wil not work yet (calls nonexistant pages)

--- DungeonLibrary/Combat.cs
+++ DungeonLibrary/Combat.cs
@@ -1,12 +1,50 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using DungeonProgram;
+using System;
 
 namespace DungeonLibrary
 {
     public class Combat
     {
-    }
-}
+        public static void Attack(CharacterBase attacker, CharacterBase defender)
+        {
+            //Generate randomNumber for dmg
+            Random rand = new Random();
+            int roll = rand.Next(1, 101);
+            System.Threading.Thread.Sleep(20);//Wait 20
+
+
+            //Determine if the Character attacked with their roll
+            if (roll <= (attacker.DetermineHitChance() - defender.DetermineBlock()))
+            {
+                int damageDone = attacker.DamageDone();
+                defender.RemainingLife -= damageDone;
+
+
+                //Change the color of the line to darkred and tell the user what happenend then rest the color to normal
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                Console.WriteLine($"{attacker.Name} struck {defender} for {damageDone} damage!");
+                Console.ResetColor();
+            }//end if
+            else
+            {
+                //Tells the user the attacker missed
+                Console.WriteLine($"{attacker} struck only air!");
+            }//end else
+        }//end Attack function
+
+        //Actual combat function
+        public static void Battle(Player player, Monster monster)
+        {
+            //Player attacks first
+            Battle(player, monster);
+            //Monster only attacks if it is still alive
+            if (monster.RemainingLife > 0)
+            {
+                Attack(monster, player);
+            }//end if
+
+        }//end battle function
+
+
+}//end class
+}//end NameSpace
